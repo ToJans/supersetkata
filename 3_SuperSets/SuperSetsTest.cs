d85d4c8b5733963f4e96e94d071abd04a2e00673
Janssens Tom <tom.janssens@cegeka.com>
2018-02-12 14:37:03 +0100

    Add test to compare 2 equal supersets with subsets

--- 3_SuperSets/SuperSetsTest.cs
+++ 3_SuperSets/SuperSetsTest.cs
@@ -35,6 +35,14 @@ namespace test
         }
 
         [TestMethod]
+        public void twoFilledSetsWithSameElementsAreEqual()
+        {
+            var leftSet = new SuperSet<int>().AddSetWithElements(1,2);
+            var rightSet = new SuperSet<int>().AddSetWithElements(1, 2);
+            Assert.AreEqual(leftSet, rightSet);
+        }
+
+        [TestMethod]
         public void superSetsOfSingleElement()
         {
             HashSet<HashSet<int>> expected = new HashSet<HashSet<int>>();
